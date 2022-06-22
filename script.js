49e485cc309a2b6105802e8c23e677939cdcc17e
+dennis kiptoo <+dennis.kiptoo@student.moringaschool.com>
2022-06-22 21:32:26 +0300

    6th commit

--- script.js
+++ script.js
@@ -19,12 +19,21 @@ const repository = document.getElementById('repo-list')
 const photo = document.getElementById('img')
 const followed=document.getElementById('followers')
 const follow =document.getElementById('following')
+const name= document.getElementById('name')
+const location= document.getElementById('loc')
+const date= document.getElementById('date')
+const company= document.getElementById('com')
+
 //   .catch((error)=>{
 //     console.log ('it is an error ')
 photo.innerHTML = `<img src = "${data.avatar_url}" >`
 repository.textContent = data.public_repos
 followed.textContent= data.followers
 follow.textContent = data.following
+name.textContent = data.name
+location.textContent = data.location
+date.textContent = data.created_at
+company.textContent = data.company
   })
 });
 
