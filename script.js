58f0f5afff36c9578348dfc00bcf3f609127359a
+dennis kiptoo <+dennis.kiptoo@student.moringaschool.com>
2022-06-22 13:00:17 +0300

    third commit

--- script.js
+++ script.js
@@ -5,16 +5,26 @@ inputForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const input = document.getElementById('searchByName');
-console.log (input.value)
+alert(input.value)
+console.log(input.value)
 
-fetch ('https://api.github.com/users/ ${input.value}')
+fetch(`https://api.github.com/users/${input.value}`)
+
+
+// fetch ('https://api.github.com/users/$[input.value]')
 .then(response => response.json())
 .then (data => {
-  console.log(data);
 
-
+const repository = document.getElementById('repo-list')
+ const img = document.getElementById('img')
+const followed=document.getElementById('followers')
+const follow =document.getElementById('following')
 //   .catch((error)=>{
 //     console.log ('it is an error ')
+img.src = data.avatar_url
+repository.innerHTML = data.public_repos
+followed.innerHTML= data.followers
+follow.innerHTML = data.following
   })
 });
 
