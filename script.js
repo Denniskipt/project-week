12610c3996a75869ddb21961390f8f36f36af675
+dennis kiptoo <+dennis.kiptoo@student.moringaschool.com>
2022-06-22 20:19:12 +0300

    fifth commit

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ console.log(input.value)
 fetch(`https://api.github.com/users/${input.value}`)
 
 
-// fetch ('https://api.github.com/users/$[input.value]')
+ fetch (`https://api.github.com/users/${input.value}`)
 .then(response => response.json())
 .then (data => {
 
 const repository = document.getElementById('repo-list')
- const img = document.getElementById('img').src
+const photo = document.getElementById('img')
 const followed=document.getElementById('followers')
 const follow =document.getElementById('following')
 //   .catch((error)=>{
 //     console.log ('it is an error ')
-img.innerHtml = data.avatar_url
+photo.innerHTML = `<img src = "${data.avatar_url}" >`
 repository.textContent = data.public_repos
 followed.textContent= data.followers
 follow.textContent = data.following
